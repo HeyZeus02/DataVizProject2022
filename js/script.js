f88810119e2f66b8284fbc9e3ced173839ec16f1
Jonathan-Himes <97470097+Jonathan-Himes@users.noreply.github.com>
2022-11-10 20:46:48 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-let mapData;
-let high_points;
-let table_data;
-let input_data;
-let visited_highpoints;
+let mapData; // This holds json with all the us states in it to be mapped
+let high_points; // This has information for each state
+let table_data; // This is the data that populates the table
+let input_data; // This is the read in csv from the user
+let visited_highpoints; // Ths is a filtered list of only highpoints that the user has visited
+let path1;
+let path2;
+let path3;
+
 
 let headerData = [{sorted: false, ascending: false},  // used in sorting the table data
                   {sorted: false, ascending: false},
@@ -12,8 +16,8 @@ let headerData = [{sorted: false, ascending: false},  // used in sorting the tab
 
 async function loadData () {
   
-    high_points = await d3.csv('data/high_points.csv');
-  mapData = await d3.json('data/us-states.json');  
+  high_points = await d3.csv('data/high_points.csv'); // Load in the csv containing data on each high point
+  mapData = await d3.json('data/us-states.json'); // Load in the json containing the state boundaries
   return {high_points, mapData};
 }
 
@@ -26,17 +30,22 @@ loadData().then((loadedData) => {
     
     for (i in high_points){
         
-        high_points[i].X = parseFloat(high_points[i].X );
+        high_points[i].X = parseFloat(high_points[i].X ); //parse the data
         high_points[i].Y = parseFloat(high_points[i].Y );
         high_points[i].Elevation = parseFloat(high_points[i].Elevation );
-   }
+    }
 
     
-    table_data = high_points;
-    read_in_user_data()
+    table_data = high_points; // Initializes the table_data to have all the high point information
+
+    // Call the functions
+    read_in_user_data();
+    create_tabs();
     lp_map();
     lp_table();
     sort_data();
+
+
 
     
 })
